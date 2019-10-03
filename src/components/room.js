40e5e00c63b944a938c7d3cec9fa11e4d04ca440
Brendan O'Keefe <c-brendan.okeefe@charter.com>
2019-10-03 09:27:14 -0600

    appending the dom from message array

--- src/components/room.js
+++ src/components/room.js
@@ -1,38 +1,59 @@
 import React from 'react';
 import history from '../history';
-import {joinRoom, message} from '../api';
+import { joinRoom, message } from '../api';
 
 class Room extends React.Component {
-    state = {
-        room: null
+  state = {
+    room: null,
+    messages: [
+      { callSign: 'briggs', message: 'reporting for duty' },
+      { callSign: 'subs', message: 'think about it' }
+    ]
+  };
+
+  constructor(props) {
+    super(props);
+    message(message => {
+      console.log(message);
+    });
+  }
+
+  componentDidMount() {
+    if (!this.props.roomName) {
+      history.push('/');
+    } else {
+      this.setState({ room: this.props.roomName });
+      joinRoom({ room: this.props.roomName, username: this.props.callSign }, room => {
+        this.appendMessage(room.username, room.message);
+      });
     }
 
-    componentDidMount(){
-        if(!this.props.roomName) {
-            history.push('/');
-        } else {
-            this.setState({room: this.props.roomName});
-            joinRoom({room: this.props.roomName,  username:this.props.callSign}, (room) => {
-                  
-                
-            });
+    this.appendMessage('DogHair', 'friggin pony up');
+  }
 
-            message((message) => {
-                console.log(message);
-                // document.getElementById('messages').append(`<p><b>${message.username}</b> ${message.message}</p>`);
-            })
-        }
-    }
+  appendMessage(callSign, message) {
+    const concatMsgs = this.state.messages.concat({ callSign: callSign, message: message });
+    this.setState({ messages: concatMsgs });
+  }
 
-    render(){
-         return (
-            <div>
-                Welcome to {this.props.roomName}
-                <div id="messages"></div>
-            </div>
-        )   
-    }
-
+  render() {
+    return (
+      <div>
+        Welcome to {this.props.roomName}
+        <div>
+          {this.state.messages.map(function(item) {
+            return (
+              <div className="messages">
+                <p>
+                  <b>{item.callSign}</b>: {item.message}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Room;
